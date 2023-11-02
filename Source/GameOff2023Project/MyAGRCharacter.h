2a0b9427444e173d48d1d0fe096f04eabf2afe26
KingParadox <tornado4d@gmail.com>
2023-11-02 17:55:46 +0100

    Player Character BP now inherits from a Base c++ class "MyAGRCharacter"

---
+++ Source/GameOff2023Project/MyAGRCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/AGRCharacter.h"
+#include "MyAGRCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEOFF2023PROJECT_API AMyAGRCharacter : public AAGRCharacter
+{
+	GENERATED_BODY()
+	
+};
