dcf02c776a2ee98a36ca85fa3542264df95b0e4b
Dan <dwoj714@gmail.com>
2023-11-12 12:52:56 -0500

    Re-implemented sprinting/blocking
    blocking keeps ending immediately for some reason though
    blocked effects/tags for sprinting probably need adjustment too

--- Source/GameOff2023Project/GE_Calculations.cpp
+++ Source/GameOff2023Project/GE_Calculations.cpp
@@ -1,8 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "GE_Calculations.h"
 #include "GASAttributeSet.h"
 #include "GASAbilitySystemComponent.h"
-#include "GE_Calculations.h"
 
 struct FDamageStatistics
 {
